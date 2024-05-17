fd5b9e2873ae0cf0f684304692d56146f6806d19
selunenashor <phong.than.kiem.771@gmail.com>
2024-05-17 20:45:41 +0700

    Create userLayout

--- src/router.jsx
+++ src/router.jsx
@@ -1,3 +1,4 @@
+import userLayout from "./layout/userLayout/userLayout";
 import { userRoutes, testRouters } from "./utils/router";
 import { Routes, Route } from "react-router-dom";
 
@@ -6,9 +7,18 @@ const RouterCustom = () => {
     <>
       <Routes>
         {userRoutes.map((route, index) => {
+          const Layout = userLayout;
           const Page = route.component;
           return (
-            <Route key={index} path={route.path} element={<Page />}></Route>
+            <Route
+              key={index}
+              path={route.path}
+              element={
+                <Layout>
+                  <Page />
+                </Layout>
+              }
+            ></Route>
           );
         })}
         {testRouters.map((route, index) => {
