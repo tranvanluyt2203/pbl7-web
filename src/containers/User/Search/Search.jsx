cec2713e41eb13ad3ff8bd186d7f9b0556260c13
selunenashor <phong.than.kiem.771@gmail.com>
2024-06-01 14:18:45 +0700

    Create Search

---
+++ src/containers/User/Search/Search.jsx
@@ -0,0 +1,47 @@
+import ProductShopFull from "../../../components/ProductShopFull/ProductShopFull";
+import "./Search.scss";
+
+const Search = () => {
+  return (
+    <>
+      <div className="result__header">
+        <div className="result__count">
+          Có <span>1.234.567</span> kết quả cho <span>điện thoại</span>
+        </div>
+        <div className="result__filter">
+          <div className="filter" id="sort">
+            <select name="sortSelect" id="sortSelect">
+              <option value="1">Giá từ cao đến thấp</option>
+              <option value="2">Giá từ thấp đến cao</option>
+            </select>
+          </div>
+          <button className="filterOpen">
+            <i className="fa-regular fa-filter"></i>
+            Bộ lọc
+          </button>
+        </div>
+      </div>
+      <div className="result__filter_detail">
+        <select name="shopFilter" id="shopFilter">
+          <option value="">Cửa hàng</option>
+          <option value="shopee.vn">shopee.vn</option>
+        </select>
+        <select name="priceFilter" id="priceFilter">
+          <option value="">Price</option>
+        </select>
+      </div>
+      <div className="result__content">
+        <ProductShopFull></ProductShopFull>
+        <ProductShopFull></ProductShopFull>
+        <ProductShopFull></ProductShopFull>
+        <ProductShopFull></ProductShopFull>
+        <ProductShopFull></ProductShopFull>
+        <ProductShopFull></ProductShopFull>
+        <ProductShopFull></ProductShopFull>
+        <ProductShopFull></ProductShopFull>
+      </div>
+    </>
+  );
+};
+
+export default Search;
--- src/utils/router.jsx
+++ src/utils/router.jsx
@@ -1,8 +1,11 @@
 import ProductBlock from "../components/ProductBlock/ProductBlock";
 import HomePage from "../containers/User/HomePage/HomePage";
+import Search from "../containers/User/Search/Search";
 
-
-const userRoutes = [{ path: "/", component: HomePage }];
+const userRoutes = [
+  { path: "/", component: HomePage },
+  { path: "/search", component: Search },
+];
 
 const testRouters = [{ path: "/test", component: ProductBlock }];
 
